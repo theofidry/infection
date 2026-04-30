ff288f73fa26731ec954ad359c9cce3276a61b5f
Théo FIDRY <theo.fidry@gmail.com>
2026-04-30 12:37:51 +0200

    fix cs

--- src/Report/Framework/Writer/FileWriter.php
+++ src/Report/Framework/Writer/FileWriter.php
@@ -1,12 +1,44 @@
 <?php
+/**
+ * This code is licensed under the BSD 3-Clause License.
+ *
+ * Copyright (c) 2017, Maks Rafalko
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 declare(strict_types=1);
 
 namespace Infection\Report\Framework\Writer;
 
-use Symfony\Component\Filesystem\Filesystem;
+use function implode;
 use function is_string;
-use function Pipeline\take;
+use function iterator_to_array;
+use Symfony\Component\Filesystem\Filesystem;
 
 final readonly class FileWriter implements ReportWriter
 {
@@ -14,14 +46,16 @@ final readonly class FileWriter implements ReportWriter
         private Filesystem $filesystem,
         private string $filePath,
     ) {
-        // TODO: validation of the path? Separate constructor?
     }
 
     public function write(iterable|string $contentOrLines): void
     {
         $contents = is_string($contentOrLines)
             ? $contentOrLines
-            : take($contentOrLines)->toList();
+            : implode(
+                "\n",
+                iterator_to_array($contentOrLines),
+            );
 
         $this->filesystem->dumpFile($this->filePath, $contents);
     }
